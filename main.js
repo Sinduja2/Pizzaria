b18af71c644b4e5d3a57abf3b0f1d31276a396d0
Sinduja2 <83639898+Sinduja2@users.noreply.github.com>
2021-07-26 18:03:32 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,37 +1,72 @@
-menu_list_array = ["Veg Margherita Pizza",
-"Chichen Tandoori Pizza",
-"Veg Suprime Pizza",
-"Paneer Tikka Pizza",
-"Dulex Veggi Pizza",
-"Veg Extravaganza Pizza"];
 
-function getmenu(){
-var htmldata;
-htmldata="<ol class='menulist'>"
-menu_list_array.sort();
-for (var i=0;i<menu_list_array.length;i++){
-    htmldata=htmldata+'<li>'+ menu_list_array[i] + '</li>' 
-}
-htmldata=htmldata + "<ol>"
-document.getElementById("display_menu").innerHTML = htmldata;
+name_of_the_student_array = [];
+    
+function submit()
+{
+    var display_student_array = [];
+
+    for (var j = 1; j <= 4; j++) 
+    {
+        var name_of_the_student = document.getElementById("name_of_the_student_"+j).value;
+        console.log(name_of_the_student);
+        name_of_the_student_array.push(name_of_the_student);
+    }
+
+    console.log(name_of_the_student_array);
+
+    var lenght_of_name_of_students_array = name_of_the_student_array.length;
+    console.log(lenght_of_name_of_students_array);
+
+    for (var k = 0; k < lenght_of_name_of_students_array; k++) 
+    {
+        display_student_array.push("<h4>NAME - "+ name_of_the_student_array[k] + "</h4>");
+        console.log(display_student_array);
+    }
+
+    console.log(display_student_array);
+    document.getElementById("display_name_with_commas").innerHTML = display_student_array;
+
+    var remove_commas = display_student_array.join(" ");
+    console.log(remove_commas);
+    document.getElementById("display_name_without_commas").innerHTML = remove_commas;
+
+
+    document.getElementById("submit_button").style.display = "none";
+    document.getElementById("sort_button").style.display = "inline-block";
+
 }
 
-function add_item(){
-var htmldata;
-var item=document.getElementById("add_item").value;
-menu_list_array.sort();
-htmldata=" <section class = 'cards'>"
-for(var i=0;i<menu_list_array.length;i++){
-    htmldata = htmldata + "<div class = 'cards'>"
-       + '<image src = "images/pizzaImg.png"/>'
-        + menu_list_array[i] + '</div>'
-        document.getElementById("display_addedmenu").innerHTML = htmldata;
+function sorting()
+{
+    name_of_the_student_array.sort();
+    console.log(name_of_the_student_array);
+
+    var display_student_array_sorting = [];
+
+    var lenght_of_name_of_students_array = name_of_the_student_array.length;
+    console.log(lenght_of_name_of_students_array);
+
+    for (var k = 0; k < lenght_of_name_of_students_array; k++) 
+    {
+        display_student_array_sorting.push("<h4>NAME - " + name_of_the_student_array[k] + "</h4>");
+        console.log(display_student_array_sorting);
+    }
+
+    var remove_commas = display_student_array_sorting.join(" ");
+    console.log(remove_commas);
+
+    document.getElementById("display_name_without_commas").innerHTML = remove_commas;
 }
 
+
+//Additional activity
+function new_update()
+{
+    document.getElementById("display_name_without_commas").innerHTML = "<h1>" + name_of_the_student_array +"</h1>";
 }
 
-function add_top(){
-var iten=document.getElementById("add_item").value;
-menu_list_array.push(item);
-add_item();
-}
+
+
+
+
+
